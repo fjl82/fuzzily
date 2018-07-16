30f77f1b6661e4bf065477596bd9eaaa95103144
Ferry Landzaat <flandzaat@elmar.nl>
2018-07-16 14:35:17 +0200

    Add rails 5.2 support

--- lib/fuzzily/searchable.rb
+++ lib/fuzzily/searchable.rb
@@ -6,9 +6,10 @@ module Fuzzily
 
     def self.included(by)
       case ActiveRecord::VERSION::MAJOR
-      when 2 then by.extend Rails2ClassMethods
-      when 3 then by.extend Rails3ClassMethods
-      when 4 then by.extend Rails4ClassMethods
+        when 2 then by.extend Rails2ClassMethods
+        when 3 then by.extend Rails3ClassMethods
+        when 4 then by.extend Rails4ClassMethods
+        when 5 then by.extend Rails4ClassMethods
       end
     end
 
