96426fd1768fb7c8ab29213972f14d8701b01673
Ferry Landzaat <flandzaat@elmar.nl>
2018-07-16 14:37:35 +0200

    Add where support.

--- lib/fuzzily/searchable.rb
+++ lib/fuzzily/searchable.rb
@@ -43,13 +43,24 @@ module Fuzzily
       def _find_by_fuzzy(_o, pattern, options={})
         options[:limit] ||= 10 unless options.has_key? :limit
         options[:offset] ||= 0
+        options[:where] ||= {} unless options.has_key? :where
+
+        if options[:where].present?
+          ids = self.name.constantize.where(options[:where]).pluck(:id)
+          if ids.count > 0
+            options[:where] = { owner_id: ids }
+          else
+            return []
+          end
+        end
 
         trigrams = _o.trigram_class_name.constantize.
-          limit(options[:limit]).
-          offset(options[:offset]).
-          for_model(self.name).
-          for_field(_o.field.to_s).
-          matches_for(pattern)
+            where(options[:where]).
+            limit(options[:limit]).
+            offset(options[:offset]).
+            for_model(self.name).
+            for_field(_o.field.to_s).
+            matches_for(pattern)
         records = _load_for_ids(trigrams.map(&:owner_id))
         # order records as per trigram query (no portable way to do this in SQL)
         trigrams.map { |t| records[t.owner_id] }.compact
